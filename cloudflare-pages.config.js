76d5bb992758ff167a1b514d3211850f8dd4c80c
ondarenc <ondarenc@gmail.com>
2026-06-17 06:58:55 +0200

    Remove cloudflare-pages.config.js - not being read by Cloudflare

--- cloudflare-pages.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  buildCommand: 'npm install && npm run build',
-  outputDirectory: 'build',
-  nodeVersion: '20',
-  environment: 'production',
-  installCommand: 'npm install'
-};
